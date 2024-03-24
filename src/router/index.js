5f1b2664d46af06690efadebaef91e56d3a9598c
Gaspor <gaspor991@gmail.com>
2024-03-24 04:42:21 -0300

    bugfix: in router don't use component: () => MyComponent

--- src/router/index.js
+++ src/router/index.js
@@ -10,27 +10,33 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/pages/Home.vue'
 import LoginView from '@/pages/Login.vue'
 import RegisterView from '@/pages/Register.vue'
+import PageNotFoundView from '@/pages/404.vue'
 
 const routes = [
   {
     path: '/',
     name: "Home",
-    component: () => HomeView,
+    component: HomeView,
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => LoginView,
+    component: LoginView,
   },
   {
     path: '/register',
     name: 'Register',
-    component: () => RegisterView,
-  }
+    component: RegisterView,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: PageNotFoundView
+  },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
